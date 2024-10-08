7bd0966c77a4b13ec69dd8f62c50a4550f3cabbd
Ngoni <ngonimapfumo@hotmail.com>
2024-10-08 23:07:05 +0200

    Initial Commit

---
+++ ChantsTests/ChantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChantsTests.swift
+//  ChantsTests
+//
+//  Created by Ngoni Mapfumo on 10/8/24.
+//
+
+import Testing
+@testable import Chants
+
+struct ChantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
